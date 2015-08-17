c47b1066d6a967e5605ad77165d6a1ff92edc72f
lilninja <hvznm10@yandex.ru>
2015-08-17 13:03:38 +0300

    Img fix

--- js/app.js
+++ js/app.js
@@ -1,38 +1,38 @@
 angular
-	.module('app',[
-		'ui.router'
+.module('app',[
+	'ui.router'
 	])
 
-	.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-		$urlRouterProvider.otherwise('/');
+.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise('/');
 
-		$stateProvider
-			.state('new', {
-				url: '/',
-				templateUrl: 'templates/new.html',
-				controller: 'RecipeListCtrl'
-			})
+	$stateProvider
+	.state('new', {
+		url: '/',
+		templateUrl: 'templates/new.html',
+		controller: 'RecipeListCtrl'
+	})
 
-			.state('popular', {
-				url: '/popular',
-				templateUrl: 'templates/popular.html',
-				controller: 'RecipeListCtrl'
-			})
+	.state('popular', {
+		url: '/popular',
+		templateUrl: 'templates/popular.html',
+		controller: 'RecipeListCtrl'
+	})
 
-			.state('season', {
-				url: '/season',
-				templateUrl: 'templates/season.html',
-				controller: 'RecipeListCtrl'
-			})
+	.state('season', {
+		url: '/season',
+		templateUrl: 'templates/season.html',
+		controller: 'RecipeListCtrl'
+	})
 
-			.state('info', {
-				url: '/info',
-        		templateUrl: 'templates/info.html'
-      		})
+	.state('info', {
+		url: '/info',
+		templateUrl: 'templates/info.html'
+	})
 
-      		.state('recipeId',{
-      			url: '/recipes/:recipeId',
-      			templateUrl: 'templates/recipe.html',
-      			controller: 'RecipeDetailCtrl'
-      		});
-	}])
+	.state('recipeId',{
+		url: '/recipes/:recipeId',
+		templateUrl: 'templates/recipe.html',
+		controller: 'RecipeDetailCtrl'
+	});
+}])
